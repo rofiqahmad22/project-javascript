710835adb0e419911e8382309be710f85446dc91
Rofiq Ahmad Mubarok <rofiqahmadm@gmail.com>
2023-01-15 05:46:29 +0700

    change prompt question

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const bloods = prompt('what was your blood pressure when you last checked? (sistonic & diastonic)')
+const bloods = prompt('what was your blood pressure when you last checked? (sistonic)')
 
 if (bloods < 120) {
     console.log('your blood pressure is normal')
 }
-else if (bloods >= 120 && bloods < 129) {
+else if (bloods >= 120 && bloods <= 129) {
     console.log('Your blood pressure is up and almost close to hypertension!')
 }
 else if (bloods > 130) {
