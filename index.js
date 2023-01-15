dd419d0ffba7abb436936b863ba3f672820d1515
Rofiq Ahmad Mubarok <rofiqahmadm@gmail.com>
2023-01-15 09:14:00 +0700

    format program

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const bloods = prompt('what was your blood pressure when you last checked? (sistonic)')
+const bloods = prompt(
+  "what was your blood pressure when you last checked? (sistonic)"
+);
 
 if (bloods < 120) {
-    console.log('your blood pressure is normal')
-}
-else if (bloods >= 120 && bloods <= 129) {
-    console.log('Your blood pressure is up and almost close to hypertension!')
-}
-else if (bloods > 130) {
-    console.log('you have hypertension :(')
+  console.log("your blood pressure is normal");
+} else if (bloods >= 120 && bloods <= 129) {
+  console.log("Your blood pressure is up and almost close to hypertension!");
+} else if (bloods > 130) {
+  console.log("you have hypertension :(");
 }
